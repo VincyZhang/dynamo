7ea9cec825a27a2f163a65b1e4bfc0676afacdd5
VincyZhang <wenxin.zhang@intel.com>
2026-05-11 01:19:54 -0700

    revert(xpu): restore upstream multimodal URL import pattern
    Revert TOCTOU-related changes to conftest.py, test_vllm_xpu.py, and
    test_vllm.py. Restore allocate_port at module level and direct import
    of MULTIMODAL_IMG_URL and get_multimodal_test_image_bytes. Also revert
    multimodal_agg_frontend_decoding back to post_merge marker and remove
    DYN_MM_ALLOW_INTERNAL env.
    
    Signed-off-by: VincyZhang <wenxin.zhang@intel.com>

--- tests/serve/test_vllm.py
+++ tests/serve/test_vllm.py
@@ -15,7 +15,7 @@ from tests.serve.common import (
     params_with_model_mark,
     run_serve_deployment,
 )
-import tests.serve.conftest as _serve_conftest
+from tests.serve.conftest import MULTIMODAL_IMG_URL, get_multimodal_test_image_bytes
 from tests.serve.lora_utils import MinioLoraConfig
 from tests.serve.multimodal_profiles.vllm import (
     VLLM_MULTIMODAL_PROFILES,
@@ -414,7 +414,7 @@ vllm_configs = {
                                 },
                                 {
                                     "type": "image_url",
-                                    "image_url": {"url": _serve_conftest.MULTIMODAL_IMG_URL},
+                                    "image_url": {"url": MULTIMODAL_IMG_URL},
                                 },
                             ],
                         }
